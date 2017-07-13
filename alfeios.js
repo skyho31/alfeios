d6fc0f0062c3ee4679b983e08fe24873ace24aea
김선호 <n3n_rever@gimseonhoui-Mac-mini.local>
2017-07-14 08:19:14 +0900

    pdf 최종 수정 및 Test용 request 주기 변경

--- alfeios.js
+++ alfeios.js
@@ -112,4 +112,4 @@ var getRiverInfo = function(url) {
 
 //setInterval에 설정된 시간마다 데이터 가져와서 전송하도록 설정
 getRiverInfo(URL);
-setInterval(function() { getRiverInfo(URL); }, 60 * 1000);
+setInterval(function() { getRiverInfo(URL); }, 10 * 1000);
